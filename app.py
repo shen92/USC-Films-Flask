df485d69758c54d8619813541f7836acb636fea1
Yingjie Shen <shen92@wisc.edu>
2021-02-27 07:11:49 -0600

    update

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import jsonify
+from flask import send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_url_path='')
+CORS(app)
+
+@app.route('/')
+def get_landing_page():
+    return app.send_static_file("index.html")
+
+@app.route('/home')
+def get_home_contents():
+    data = {"message": "this is info for home"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
