e6805d23829a5f7c91fd40af025636340c53d96a
Yingjie Shen <shen92@wisc.edu>
2021-02-28 01:32:56 -0600

    deploy build main*@b2245a1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 pip freeze > requirements.txt
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+}
+
+# gets the current git branch
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+}
+
+# DEMO
+GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+
 git add .
-git commit -m 'deploy latest build'
+git commit -m "deploy build ${GIT_BRANCH}"
 git push
