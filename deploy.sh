059915f16a323c16a9fafe4d3dab42fb52e45a4b
Yingjie Shen <shen92@wisc.edu>
2021-02-28 01:39:05 -0600

    deploy build from 074d9a0

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 pip freeze > requirements.txt
 current_git_branch_latest_short_id=`git rev-parse --short HEAD`
 git add .
-git commit -m "deploy build ${current_git_branch_latest_short_id}"
+git commit -m "deploy build from ${current_git_branch_latest_short_id}"
 git push
