01ebb7303fc1c80b186a2d23ba141490c8f73dc7
Yingjie Shen <shen92@wisc.edu>
2021-02-27 22:03:26 -0600

    add test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from flask import send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
@@ -15,9 +15,9 @@ def get_home_contents():
     data = {"message": "this is info for home"}
     return jsonify(data)
 
-# @add.route('/test')
-# def test():
-#     return 'test'
+@app.route('/test')
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run()
